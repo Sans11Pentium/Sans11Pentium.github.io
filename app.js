f729ae39927c1684421d19f62350f776247f241e
Sans11Pentium <146310722+Sans11Pentium@users.noreply.github.com>
2023-12-31 19:35:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Frontend development", "backend development", "Web designing", "Coding"];
+const textArray = ["Frontend development", "Backend development", "Web designing", "Coding"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
